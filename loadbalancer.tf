e012035b102d35ff8f8ca97b00e89a2af24ca464
Nevralgie <tomrenard69@gmail.com>
2023-05-17 09:38:59 +0200

    update commit

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,4 +1,4 @@
-resource "azurerm_public_ip" "test" {
+ resource "azurerm_public_ip" "test" {
    name                         = "pubIPLB"
    location                     = azurerm_resource_group.test.location
    resource_group_name          = azurerm_resource_group.test.name
@@ -16,7 +16,37 @@ resource "azurerm_public_ip" "test" {
    }
  }
 
+#Create Loadbalancing Rules
+resource "azurerm_lb_rule" "production-inbound-rules" {
+  loadbalancer_id                = azurerm_lb.test.id
+  resource_group_name            = azurerm_resource_group.test.name
+  name                           = "ssh-inbound-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = 22
+  backend_port                   = 22
+  frontend_ip_configuration_name = "publicIPAddress"
+  probe_id                       = azurerm_lb_probe.ssh-inbound-probe.id
+  backend_address_pool_ids        = ["${azurerm_lb_backend_address_pool.test.id}"]
+ }
+
+#Create Probe
+resource "azurerm_lb_probe" "ssh-inbound-probe" {
+  resource_group_name = azurerm_resource_group.test.name
+  loadbalancer_id     = azurerm_lb.test.id
+  name                = "ssh-inbound-probe"
+  port                = 22
+}
+
  resource "azurerm_lb_backend_address_pool" "test" {
    loadbalancer_id     = azurerm_lb.test.id
    name                = "BackEndAddressPool"
  }
+
+#Automated Backend Pool Addition > Gem Configuration to add the network interfaces of the VMs to the backend pool.
+resource "azurerm_network_interface_backend_address_pool_association" "test" {
+  count                   = 2
+  network_interface_id    = azurerm_network_interface.test.*.id[count.index]
+  ip_configuration_name   = azurerm_network_interface.test.*.ip_configuration.0.name[count.index]
+  backend_address_pool_id = azurerm_lb_backend_address_pool.test.id
+
+}
